8e6b5467498dfede3b0e6834a83fd3fd7e8db72f
Prashant Pandey <prashant.prashn@gmail.com>
2018-03-21 17:38:56 -0400

    Adding benchmarking code and zipfian generation code.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-TARGETS=main
+TARGETS=main bm
 
 ifdef D
 	DEBUG=-g
@@ -18,8 +18,8 @@ ifdef P
 	PROFILE=-pg -no-pie # for bug in gprof.
 endif
 
+CC = gcc -std=gnu11
 CXX = g++ -std=c++11
-CC = g++ -std=c++11
 LD= g++ -std=c++11
 
 CXXFLAGS = -Wall $(DEBUG) $(PROFILE) $(OPT) $(ARCH) -m64 -I. -Wno-unused-result -Wno-unused-function -Wno-strict-aliasing
@@ -34,24 +34,25 @@ all: $(TARGETS)
 
 # dependencies between programs and .o files
 
-main:                  main.o 	gqf.o	hashutil.o
+main:                  main.o gqf.o	hashutil.o
+bm:                    bm.o gqf.o	zipf.o hashutil.o
 
 # dependencies between .o files and .h files
 
-main.o: 								 									gqf.h
-hashutil.o: 															hashutil.h
+main.o: 								gqf.h
+bm.o:										gqf_wrapper.h
 
 # dependencies between .o files and .cc (or .c) files
 
 %.o: %.cc
-gqf.o: gqf.c gqf.h
+%.o: %.c
 
 #
 # generic build rules
 #
 
 $(TARGETS):
-	$(LD) $^ $(LDFLAGS) -o $@
+	$(LD) $^ -o $@ $(LDFLAGS)
 
 %.o: %.cc
 	$(CXX) $(CXXFLAGS) $(INCLUDE) $< -c -o $@
---
+++ gqf_wrapper.h
@@ -0,0 +1,83 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  qf_wrapper.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  10/28/2015 04:48:55 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Prashant Pandey (ppandey@cs.stonybrook.edu), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef GQF_WRAPPER_H
+#define GQF_WRAPPER_H
+
+#include "gqf.h"
+
+QF* g_quotient_filter;
+QFi g_quotient_filter_itr;
+
+extern inline int gqf_init(uint64_t nbits, uint64_t num_hash_bits)
+{
+	uint64_t nslots = 1 << nbits;
+	g_quotient_filter = qf_malloc(nslots, num_hash_bits, 0, LOCKS_FORBIDDEN,
+																NONE, 0);
+	return 0;
+}
+
+extern inline int gqf_insert(__uint128_t val, uint64_t count)
+{
+	qf_insert(g_quotient_filter, val, 0, count);
+	return 0;
+}
+
+extern inline int gqf_lookup(__uint128_t val)
+{
+	return qf_count_key_value(g_quotient_filter, val, 0);
+}
+
+extern inline __uint128_t gqf_range()
+{
+	return g_quotient_filter->metadata->range;
+}
+
+extern inline int gqf_destroy()
+{
+	qf_free(g_quotient_filter);
+	return 0;
+}
+
+extern inline int gqf_iterator(uint64_t pos)
+{
+	qf_iterator(g_quotient_filter, &g_quotient_filter_itr, pos);
+	return 0;
+}
+
+/* Returns 0 if the iterator is still valid (i.e. has not reached the
+ * end of the QF. */
+extern inline int gqf_get(uint64_t *key, uint64_t *value, uint64_t *count)
+{
+	return qfi_get(&g_quotient_filter_itr, key, value, count);
+}
+
+/* Advance to next entry.  Returns whether or not another entry is
+ * found.  */
+extern inline int gqf_next()
+{
+	return qfi_next(&g_quotient_filter_itr);
+}
+
+/* Check to see if the if the end of the QF */
+extern inline int gqf_end()
+{
+	return qfi_end(&g_quotient_filter_itr);
+}
+
+#endif
