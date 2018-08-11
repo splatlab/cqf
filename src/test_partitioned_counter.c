78f4e9947448f6f516b0f8f62d9cb9c7e8c1cc35
Prashant Pandey <prashant.prashn@gmail.com>
2018-08-11 00:16:02 -0700

    more benchmarking.

--- src/test_partitioned_counter.c
+++ src/test_partitioned_counter.c
@@ -15,6 +15,8 @@
 #include <linux/unistd.h>
 #include <sys/syscall.h>
 #include <errno.h>
+#include <time.h>
+#include <sys/time.h>
 
 #include "partitioned_counter.h"
 
@@ -24,15 +26,24 @@
 #define DEC_TO INC_BY/2 // one million...
 #define INC_BY 100 // one million...
 
+uint64_t TOTAL_COUNT;
+
+uint64_t tv2msec(struct timeval tv)
+{
+	return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
 void *thread_routine(void *arg) {
 	pc_t *pc_counter = (pc_t*)arg;
 
-	for (int i = 0; i < INC_TO; i++) {
+	/*for (int i = 0; i < INC_TO; i++) {*/
+		/*pc_add(pc_counter, INC_BY);*/
+	/*}*/
+	/*for (int i = 0; i < DEC_TO; i++) {*/
+		/*pc_add(pc_counter, -INC_BY);*/
+	/*}*/
+	for (uint64_t i = 0; i < TOTAL_COUNT; i++)
 		pc_add(pc_counter, INC_BY);
-	}
-	for (int i = 0; i < DEC_TO; i++) {
-		pc_add(pc_counter, -INC_BY);
-	}
 
 	return NULL;
 }
@@ -52,7 +63,9 @@ int main (int argc, char *argv[])
 		return 1;
 	}
 	int procs = atoi(argv[1]);
+	TOTAL_COUNT =  (1ULL << 24) / procs;
 
+	struct timeval start, stop;
 	pc_init(&pc_counter, &global_counter, 8, 100);
 
 	pthread_t *thrs = malloc( sizeof( pthread_t ) * procs);
@@ -63,6 +76,7 @@ int main (int argc, char *argv[])
 	}
 	printf( "Starting %d threads...\n", procs );
 
+	gettimeofday(&start, NULL);
 	for (int i = 0; i < procs; i++) {
 		if (pthread_create(&thrs[i], NULL, thread_routine, (void *)(&pc_counter)))
 		{
@@ -75,12 +89,15 @@ int main (int argc, char *argv[])
 		pthread_join( thrs[i], NULL );
 
 	pc_sync(&pc_counter);
+	gettimeofday(&stop, NULL);
 
 	free(thrs);
+	printf("time: %ld\n", tv2msec(stop) - tv2msec(start));
 
 	printf("After doing all the math, global_int value is: %ld\n",
 				 global_counter);
-	int64_t exp_count = (INC_TO - DEC_TO) * INC_BY * procs;
+	/*int64_t exp_count = (INC_TO - DEC_TO) * INC_BY * procs;*/
+	int64_t exp_count = TOTAL_COUNT * INC_BY * procs;
 	printf("Expected value is: %ld\n", exp_count);
 	if (global_counter != exp_count)
 		printf("Counting failed!\n");
