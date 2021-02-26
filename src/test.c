fde284387edc5fb2f27a81ad366ea18eeb8ec17d
Eric Wimsatt <ericwimsatt@gmail.com>
2021-02-25 20:31:45 -0800

    made stable

--- src/test.c
+++ src/test.c
@@ -40,7 +40,8 @@ int main(int argc, char **argv)
 	//I think this is number of slots per quotient
 	uint64_t nslots = (1ULL << qbits);
 	//this can be changed to change the % it fills up
-	uint64_t nvals =10*nslots/100;
+
+	uint64_t nvals = 80*nslots/100;
 	uint64_t key_count = 1;
 	uint64_t *vals;
 	uint64_t* hashes;
